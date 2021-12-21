c0d2cbb870afc81aef5edc29a8eae88d98595b21
BALABHASKARARAO <balabhaskararaoguntupalli@gmail.com>
2021-12-21 12:45:45 +0530

    mait.ff update-7

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ resource "aws_s3_bucket_object" "base_folder" {
 }
 ## the below is upload file to s3 bucket
 resource "aws_s3_bucket_object" "uploadfile" {
-  bucket = "{var.s3_bucketname${var.folder_name}/}"
-  key     = "index.html"
+  bucket = var.s3_bucketname
+  key     = "${var.folder_name}/index.html"
   source = "./index.html"
   depends_on = [aws_s3_bucket.testbucket, aws_s3_bucket_object.base_folder]
 }
