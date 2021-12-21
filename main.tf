8a4ec34cd5f31a4e874b2594ab05d70e0b4a857a
BALABHASKARARAO <balabhaskararaoguntupalli@gmail.com>
2021-12-21 12:42:32 +0530

    maint.f upfate4

--- main.tf
+++ main.tf
@@ -6,13 +6,15 @@ resource "aws_s3_bucket_object" "base_folder" {
     bucket  = "${aws_s3_bucket.testbucket.id}"
     acl     = "private"
     key     =  "${var.folder_name}/"
+    
+    depends_on = [aws_s3_bucket.testbucket]
 }
 ## the below is upload file to s3 bucket
 resource "aws_s3_bucket_object" "uploadfile" {
   bucket = "{var.s3_bucketname/${var.folder_name}/}"
   key     = "index.html"
   source = "./index.html"
-  dependens_on = [aws_s3_bucket.testbucket, aws_s3_bucket_object.base_folder]
+  depends_on = [aws_s3_bucket.testbucket, aws_s3_bucket_object.base_folder]
 }
 
 
