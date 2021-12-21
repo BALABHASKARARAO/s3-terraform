26e423cc6d91b4303ff05a40fcaeee83b31862a9
BALABHASKARARAO <balabhaskararaoguntupalli@gmail.com>
2021-12-21 12:33:21 +0530

    main.tf ipated-1

--- main.tf
+++ main.tf
@@ -6,8 +6,6 @@ resource "aws_s3_bucket_object" "base_folder" {
     bucket  = "${aws_s3_bucket.testbucket.id}"
     acl     = "private"
     key     =  "${var.folder_name}/"
-    content_type = "application/x-directory"
-    kms_key_id = "key_arn_if_used"
 }
 ## the below is upload file to s3 bucket
 resource "aws_s3_bucket_object" "uploadfile" {
